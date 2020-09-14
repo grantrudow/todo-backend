3c5c7434c4d280e7d25b5faeee96171a817d1cab
grantrudow <webdev.grant@gmail.com>
2020-09-14 17:21:54 -0500

    finished mongodb integration/postman test

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const mongoose = require('mongoose');
 const todoRoutes = express.Router();
 const PORT = 4000;
 
+let Todo = require('./todo.model');
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/todos', todoRoutes);
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos', {
 	useNewUrlParser: true
@@ -19,6 +21,7 @@ connection.once('open', function() {
 	console.log('MongoDB database connection established successfully')
 })
 
+// Deliver all available todos
 todoRoutes.route('/').get(function(req, res) {
 	Todo.find(function(err, todos) {
 		if (err) {
@@ -28,6 +31,50 @@ todoRoutes.route('/').get(function(req, res) {
 		}
 	})
 })
+
+// Retrieve specifc todo by ID
+todoRoutes.route('/:id').get((req, res) => {
+	let id = req.params.id;
+	Todo.findById(id, (err, todo) => {
+		res.json(todo);
+	})
+})
+
+// Add new todo
+todoRoutes.route('/add').post((req, res) => {
+	let todo = new Todo(req.body);
+	todo.save()
+		.then(todo => {
+			res.status(200).json({
+				'todo': 'todo added successfully'
+			});
+		})
+		.catch(err => {
+			res.status(400).send('Adding new todo failed');
+		});
+});
+
+// Update a todo at a specific ID
+todoRoutes.route('/update/:id').post((req, res) => {
+	Todo.findById(req.params.id, (err, todo) => {
+		if (!todo) {
+			res.status(400).send('Data is not found');
+		} else {
+			todo.todo_description = req.body.todo_description;
+			todo.todo_responsible = req.body.todo_responsible;
+			todo.todo_completed = req.body.todo_completed;
+
+			todo.save().then(todo => {
+				res.json('Todo updated!');
+			})
+			.catch(err => {
+				res.status(400).send('Update not possible')
+			})
+		}
+	})
+})
+
+app.use('/todos', todoRoutes);
 
 app.listen(PORT, function() {
 	console.log('Server is running on PORT: ' + PORT)
